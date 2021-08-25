0a6ba2d255bf4f09c9ac52ecfaeb56bf24b569a1
Alonso <49422627+GordoEnForma@users.noreply.github.com>
2021-08-25 10:48:53 -0500

    Terminado el lab2
    Se completo el pdf del laboratorio 2 con el dataset de Titanic

---
+++ Unidad 1/Hello World.R
@@ -0,0 +1,9 @@
+print("Jelou World")
+x <- 10
+print(x)
+genero <- c(1,1,2,1,2,2,2,1,2)
+ingreso <- c(100000,300000,500000,340000,300000,500000,650000,410000,750000)
+acuerdo <- c(1,1,3,2,4,1,5,3,2)
+
+aborto <-data.frame(genero,ingreso,acuerdo)
+View(aborto)
